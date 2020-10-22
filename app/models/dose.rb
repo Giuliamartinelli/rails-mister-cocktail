a1a2d30a4c3d0dc6e2a6d91653253f46c949ba84
Giulia Martinelli <giulia.martinelli.uni@gmail.com>
2020-10-22 16:39:50 +0200

    green rake

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :ingredient, :cocktail, uniqueness: { scope: [:ingredient, :cocktail] }
   validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :doses do
-    resources :cocktails
-    resources :ingredients
+  root to: 'cocktails#index'
+   resources :cocktails do
+    resources :doses, only: [:new, :create, :edit]
   end
+  resources :doses, only: [:destroy]
 end
